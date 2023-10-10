38cd378d9b923ec5a4c70a44dea18e3ea2dfef7e
Kyle Simmons <simmonskyleb@gmail.com>
2023-10-10 04:52:13 -0500

    "technically correct", the best kind

--- main.py
+++ main.py
@@ -12,44 +12,55 @@ import copy
 directions = [(1,0),(0,1),(-1,0),(0,-1)]
 
 # Create an empty img of width x height
-img_height = 6
-img_width = 6
+img_height = 30
+img_width = 60
 img = [[0 for _ in range(img_width)] for _ in range(img_height)]
 # Clone the mask here to keep it as all "0's"
 mask = copy.deepcopy(img)
 
-# Draw a random shape and pick a random, valid starting point
-shape_size = (random.randint(3, img_width), random.randint(3, img_height))
-shape_init = (random.randint(0, img_width - shape_size[0]), random.randint(0, img_height - shape_size[1]))
+init_px = (img_width // 2, img_height // 2)
 
-# Draw a random continuous shape of color 1
-for x in range(shape_init[0], shape_init[0] + shape_size[0]):
-    for y in range(shape_init[1], shape_init[1] + shape_size[1]):
-        img[y][x] = 1
+def continuous_fill(img, img_height, img_width, pixel):
+    x, y = pixel
+    if (0 <= x < img_width
+    and 0 <= y < img_height
+    and img[y][x] == 0):
+        if random.randint(0, 10) > 3:
+            img[y][x] = 1
+            continuous_fill(img, img_height, img_width, (x+1, y))
+            continuous_fill(img, img_height, img_width, (x-1, y))
+            continuous_fill(img, img_height, img_width, (x, y+1))
+            continuous_fill(img, img_height, img_width, (x, y-1))
+        else:
+            img[y][x] = 2
+            return
 
-# Starting pixel and color must be within the shape
-# For now, just try to start in the center of the shape
-initial_pixel = (shape_init[0] + (shape_size[0] // 2), shape_init[1] + (shape_size[1] // 2))
-boundary_color = img[initial_pixel[1]][initial_pixel[0]]
+# Set the initial pixel to correct color
+x, y = init_px
+img[y][x] = 1
+continuous_fill(img, img_height, img_width, (x+1, y))
+continuous_fill(img, img_height, img_width, (x-1, y))
+continuous_fill(img, img_height, img_width, (x, y+1))
+continuous_fill(img, img_height, img_width, (x, y-1))
 
 print("Image:")
 for line in img:
     print(line)
 
-# Initialize tracing
-mask, initial_pixel, directions = bounds.init_trace(img, mask, img_width, img_height, directions, initial_pixel, boundary_color)
+# # Initialize tracing
+# mask, initial_pixel, directions = bounds.init_trace(img, mask, img_width, img_height, directions, initial_pixel, boundary_color)
 
-# Start main trace
-mask = bounds.main_trace(img, mask, img_width, img_height, directions, initial_pixel, initial_pixel, boundary_color)
+# # Start main trace
+# mask = bounds.main_trace(img, mask, img_width, img_height, directions, initial_pixel, initial_pixel, boundary_color)
 
-print("Mask:")
-for line in mask:
-    print(line)
+# print("Mask:")
+# for line in mask:
+#     print(line)
 
-# Flood fill
-img = flood.fill(img, mask, directions, img_height, img_width, initial_pixel, 3)
+# # Flood fill
+# img = flood.fill(img, mask, directions, img_height, img_width, initial_pixel, 3)
 
-print("Filled:")
-for line in img:
-    print(line)
+# print("Filled:")
+# for line in img:
+#     print(line)
 
